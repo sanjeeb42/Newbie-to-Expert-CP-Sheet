449f160e72f0a1547a6061f3e4e2317758360b87
Sanjeeb Kumar Rai <88538586+sanjeeb42@users.noreply.github.com>
2024-09-07 14:31:45 +0530

    Create Consecutive Sum Riddle.cpp

---
+++ Consecutive Sum Riddle.cpp
@@ -0,0 +1,28 @@
+//https://codeforces.com/contest/1594
+
+//Jai Shree Ram
+//Jai Maa Durga
+//Ради вашего спокойствия не пытайтесь понять все.
+ 
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+ 
+void solve(){
+   ll n;cin>>n;
+   ll r=n, l=-1*n +1;
+   cout<<l<<" "<<r<<endl;
+}
+ 
+int main()
+{
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+ 
+    return 0;
+}
